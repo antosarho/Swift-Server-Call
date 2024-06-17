90202a48366968764cad4c5f533442dc8332029b
Antonios Sarhanis <antonios@adnuntius.com>
2024-06-18 09:49:50 +1000

    Initial Commit

---
+++ Swift Server Call/Swift_Server_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Server_CallApp.swift
+//  Swift Server Call
+//
+//  Created by anto on 18/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Server_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
